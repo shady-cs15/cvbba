baadd302a4f6e3215a3f8b1d17997d61cfdf463a
Satyaki Chakraborty <satyaki.cs15@gmail.com>
2018-03-26 16:54:05 -0400

    support for regions of rejections

--- modify.py
+++ modify.py
@@ -5,6 +5,7 @@ import numpy as np
 
 video = sys.argv[1]
 annot = json.load(open(sys.argv[2]))
+out_annot = sys.argv[3]
 
 color_map = [(0, 255, 0),(0, 0, 255), (255, 0, 0)]
 
@@ -24,35 +25,57 @@ def trackbar_callback(arg):
 	frame_no = arg
 
 cv2.namedWindow('frame')
-cv2.createTrackbar('frame#','frame', 0, len(frames), trackbar_callback)
+cv2.createTrackbar('frame#','frame', 0, len(frames)-1, trackbar_callback)
+
+rors = {}
+bors = {}
 
 accept_annot = [True]*10
 frame_no = 0
+obj_no = 0
+obj_map = {}
+	
 while frame_no < len(frames):
 	frame = frames[frame_no].copy()
 	cur_objs = annot[frame_no].keys()
 
-	obj_no = 0
-	obj_map = {}
 	for cur_obj in annot[frame_no]:
-		obj_map[cur_obj] = obj_no
+		if cur_obj not in obj_map:
+			obj_map[cur_obj] = obj_no
+			obj_no +=1
+
+		# create entry in rors
+		if cur_obj not in rors:
+			rors[cur_obj]=(-1, -1)
+
 		if accept_annot[obj_no]==True:
 			cur_box = annot[frame_no][cur_obj]
 			pt1 = [int(cur_box['x']), int(cur_box['y'])]
 			pt2 = [int(cur_box['x'] + cur_box['w']), int(cur_box['y'] + cur_box['h'])]
 			cv2.rectangle(frame, tuple(pt1), tuple(pt2), color_map[obj_map[cur_obj]], 2)
-		obj_no += 1			
 
 	cv2.imshow('frame', frame)
 	k = cv2.waitKey(0)
-	if k==ord('a'):
+
+	if k==ord('r') and frame_no < len(annot):
 		for cur_obj in annot[frame_no]:
-			print 'frame:', frame_no, 'accept annotations for', cur_obj, '? (y/n)',
+			print 'frame:', frame_no, 'reject annotations for', cur_obj, '? (y/n)',
 			choice = raw_input()
-			if choice =='n':
-				accept_annot[obj_map[cur_obj]] = False
-			if choice == 'y':
-				accept_annot[obj_map[cur_obj]] = True
+			if choice =='y':
+				if accept_annot[obj_map[cur_obj]] is True:
+					bors[cur_obj] = frame_no
+					accept_annot[obj_map[cur_obj]] = False
+			if choice == 'n':
+				if accept_annot[obj_map[cur_obj]] is False:
+					print 'rejection region:', bors[cur_obj], '->', frame_no
+					accept_annot[obj_map[cur_obj]] = True
+					rors[cur_obj]=(bors[cur_obj], frame_no)
+
+		# update rors
+		for obj in rors:
+			for rej_id in range(rors[obj][0], rors[obj][1]+1):
+				annot[rej_id].pop(obj, None)
+
 	
 	if k==ord('b'):
 		if frame_no > 0:
@@ -60,6 +83,13 @@ while frame_no < len(frames):
 	if k==27:
 		break
 	if k==ord(' '):
-		frame_no +=1
+		if frame_no < len(frames)-1:
+			frame_no +=1
 	cv2.setTrackbarPos('frame#', 'frame', frame_no)
+
+print 'Accept modified annotations? (y/n)',
+choice = raw_input()
+if choice =='y':
+	with open(out_annot, 'w') as outfile:
+		json.dump(annot, outfile)
 
